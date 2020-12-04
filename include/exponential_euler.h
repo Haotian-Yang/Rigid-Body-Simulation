57df2538df73669c1d4d9afb8293e58299384682
Haotian Yang <haotian.ut@gmail.com>
2020-12-04 02:22:52 -0500

    cleaned

--- include/exponential_euler.h
+++ include/exponential_euler.h
@@ -21,32 +21,21 @@
 //  qdot - updated generalized velocities 
 inline void exponential_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, 
                             std::vector<Eigen::Matrix66d> &masses, Eigen::Ref<const Eigen::VectorXd> forces) {
-    //std::cout<< masses.size() << std::endl;
-    //std::cout<< qdot.size() << std::endl;
+
     Eigen::Matrix3d Rt, RIR;
     Rt.setZero();
     RIR.setZero();
     Rt = Eigen::Map<const Eigen::Matrix3d>(q.segment<9>(0).data());
-    //Rt.block(0, 0, 3, 1) = q.segment(0, 3);
-    //Rt.block(0, 1, 3, 1) = q.segment(3, 3);
-    //Rt.block(0, 2, 3, 1) = q.segment(6, 3);
+
 
     RIR = Rt * masses[0].block(0, 0, 3, 3) * Rt.transpose();
-    //std::cout<< "RIR" <<RIR << std::endl;
-
     double m = masses[0].block(3, 3, 3, 3)(0, 0);
-    //std::cout<< "m\n" << m << std::endl;
-
-    //std::cout<< Rt << std::endl;
 
     Eigen::Vector3d pdot, pt, omega, torques_ext, f_ext;
-
     pt = q.segment(9, 3);
     omega = qdot.segment(0, 3);
-    //std::cout<< "omega\n" << omega << std::endl;
     pdot = qdot.segment(3, 3);
-    //std::cout<< "qdot\n" << qdot << std::endl;
-    //std::cout<< "q\n" << q << std::endl;
+
 
 
     torques_ext = forces.segment(0, 3);
@@ -55,10 +44,7 @@ inline void exponential_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double 
     //Velocity update
     Eigen::Vector3d omega_new, pdot_new;
     omega_new << omega - (RIR.inverse()*(dt*omega.cross(RIR * omega) + dt*torques_ext));
-    //omega_new << 3, 3, 3;
     pdot_new << pdot + dt*f_ext/m;
-    //pdot_new << 3, 3, 3;
-    //std::cout<< "dt*f_ext/m\n" << dt*f_ext/m << std::endl;
     qdot.segment(0, 3) = omega_new;
     qdot.segment(3, 3) = pdot_new;
 
@@ -78,16 +64,7 @@ inline void exponential_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double 
     q.segment(6, 3) = R_new.block(0, 2, 3, 1);
     q.segment(9, 3) = p_new;
 
-    //Eigen::Vector3d q_new;
-    //rigid_to_world(q_new, R_new, p_new, q_t);
-    //std::cout<< "q_new" << q_new << std::endl;
-    //std::cout<< "q_t" << q_t << std::endl;
 
-
-    //std::cout<< "R_new" <<R_new << std::endl;
-    //std::cout<< "P_new" <<p_new << std::endlpdot;
-
-    //exit(0);
 
 
 }
--- src/rigid_body_jacobian.cpp
+++ src/rigid_body_jacobian.cpp
@@ -6,10 +6,6 @@ void rigid_body_jacobian(Eigen::Matrix36d &J,
                          Eigen::Ref<const Eigen::Vector3d> X) {
 
     Eigen::Matrix3d X_skew;
-   // X_skew << 0.0, -(X(2)-p(2)), X(1)-p(1),
-   //           X(2)-p(2), 0.0, -(X(0)-p(0)),
-   //           -(X(1)-p(1)), X(0)-p(0), 0.0;
-
     X_skew << 0.0, -X(2), X(1),
             X(2), 0.0, -X(0),
             -X(1), X(0), 0.0;
@@ -17,7 +13,6 @@ void rigid_body_jacobian(Eigen::Matrix36d &J,
     J.setZero();
     J.block(0, 0, 3, 3) = R * X_skew.transpose() * R.transpose();
     J.block(0, 3, 3, 3) = Eigen::Matrix3d::Identity();
-    //std::cout << "J\n" << J << std::endl;
-   
+
 }
 
